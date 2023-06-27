4cb902786160f79e0f9d5713a53883bcb768380c
JeronimoMiras <113709509+JeroSampayo@users.noreply.github.com>
2023-06-27 13:33:19 +0200

    Update client.c

--- client.c
+++ client.c
@@ -11,13 +11,61 @@
 /* ************************************************************************** */
 #include <signal.h>
 #include <stdlib.h>
+#include "minitalk.h"
 int main(int ac, char **av)
 {
-
+	pid_t	pid;
 	if (ac == 2)
 	{
+		if (!valid_pid(argv[1]))
+		return (0);
+		pid = ft_atoi(argv[1]);
+		char_to_byte(argv[2], pid);
+		ft_printf("done :)\n");
+		return (0);
+		
+		
 		kill(atoi(av[1]), SIGUSR1);
 		kill(atoi(av[1]), SIGUSR2);
 	}
 	return(0);
+}
+
+int	valid_pid(char *s)
+{
+	int	i;
+
+	i = -1;
+	while (s[++i])
+		if (!ft_isdigit(s[i]))
+			return (0);
+	if (ft_strlen(s) < 3 || ft_strlen(s) > 5)
+		return (0);
+	return (1);
+}
+
+void	char_to_byte(char *s, pid_t pid)
+{
+	int	i;
+	int	num_bit;
+
+	i = -1;
+	while (s[++i])
+	{
+		num_bit = -1;
+		while (++num_bit < 8)
+		{
+			if ((s[i] & (128 >> num_bit)) == 0)
+			{
+				if (kill(pid, SIGUSR2) == -1)
+					exit(1);
+			}
+			else
+			{
+				if (kill(pid, SIGUSR1) == -1)
+					exit(1);
+			}
+			usleep(150);
+		}
+	}
 }
